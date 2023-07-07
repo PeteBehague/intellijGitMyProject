d1fd3f321236243b17fbec2310f1f30eace75913
PeteBehague <86470946+PeteBehague@users.noreply.github.com>
2023-07-07 15:52:06 +0100

    Add files via upload

---
+++ src/main/java/Banana.java
@@ -0,0 +1,13 @@
+public class Banana{
+    public String peel(){
+        return "I'm being peeled and it hurts!";
+    }
+
+    public String addIceCream(){
+        return "Ice cream added!";
+    }
+
+    public String slip(){
+        return "I'm being slippery!";
+    }
+}
